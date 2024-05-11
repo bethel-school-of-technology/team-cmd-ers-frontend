5a48f29992879ad57648b2cfb58386ef3b7d13a5
JordanBoller <Jordanboller@yahoo.com>
2024-05-11 10:32:05 -0400

    Added Signup component and improved home page

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AboutComponent } from './about/about.component';
 import { HomeComponent } from './home/home.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 
 const routes: Routes = [
   { path: "home", component: HomeComponent },
-  { path: "about", component: AboutComponent }
+  { path: "about", component: AboutComponent },
+  { path: "signup", component: SignUpComponent }
 ];
 
 @NgModule({
