fabab6b79f23cdc34166c1ec5c7b2a410f88ca2e
Jon Ford <fjordlings605@gmail.com>
2024-05-22 11:19:03 -0500

    initial user-services - login & signup

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,7 @@
 import { Injectable } from '@angular/core';
 import { User } from '../models/user';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { tap } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -12,15 +13,22 @@ export class UserService {
   constructor(private http: HttpClient) { }
 
   signUp(newUser: User){
-
+    return this.http.post(`${this.baseURL}/signup`, newUser);
   }
 
   login(userEmail: string, password:string) {
+    let queryParams = new HttpParams();
+    queryParams = queryParams.append('email', userEmail);
+    queryParams = queryParams.append('password', password);
 
+    return this.http.get(`${this.baseURL}/login`, {params: queryParams, responseType: 'text'})
+    .pipe(tap((response:any) => {
+      localStorage.setItem('myCoffeeToken', response);
+    }));
   }
 
   editUser(editUser: User){
-    
+
   }
 
 }
